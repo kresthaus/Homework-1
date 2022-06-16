b605c2df420afbd3904880ab72705df94b13fc00
Kalininskiy Andrei <kresthaus@mail.ru>
2022-06-16 17:39:38 +0400

    Задание № 2

--- Home № 2/Program.cs
+++ Home № 2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine();
+
+int Number = new Random().Next(100, 1000);
+int NewNumber = 0;
+
+int NumberWithoutSecDig()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+
+NumberWithoutSecDig();
+
+Console.WriteLine();
+Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+Console.WriteLine();
